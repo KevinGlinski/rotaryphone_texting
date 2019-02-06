2d2fcf3debaa74a4aef4c12b9cba8709b4ce8bef
Kevin Glinski <kevin.glinski@inin.com>
2019-02-06 09:53:29 -0500

    ui work

--- game.py
+++ game.py
@@ -1,3 +1,26 @@
+import pyxel
+from guizero import App, Text, Window
+from random import randint
+
+phrases = [
+    'hi how r',
+    'hi how r u today',
+    'bonjour said Olivier',
+    
+    "If I'm not back in five minutes just wait longer",
+    # "If Stu chews shoes should Stu choose the shoes he chews",
+    "Four furious friends fought for the phone",
+
+
+]
+
+
+font = "Times New Roman"
+font_size = 85
+phrase_color = "lightblue"
+colors = ['white', '#a5c13e','#c1973e','#aaaaa9']
+main_font_color = "#ff4f1f"
+
 
 class Player:
     index = 0
@@ -8,6 +31,10 @@ class Player:
     def __init__(self, index, target_message, digit_gpio_pin, hook_gpio_pin):
         self.index = index
         self.target_message = target_message
+
+        self.text = Text(app, text="Player {}".format(index + 1), size=font_size, font=font, color=colors[index], grid=[0,(index +1) * 2, 3,1], align='left') 
+        self.text = Text(app, text=phrases[0]+ "_", size=font_size, font=font, color=colors[index], grid=[0,((index +1) * 2) + 1, 3,1],align='left') 
+        
         
     def digit_received(self, digit):
         if on_hook:
@@ -37,7 +64,52 @@ class Player:
         print('off hook')
         self.on_hook = False
         
+    def __del__(self):
+        self.text.set("die")
 
+     
 message = 'test message'
 
-Player(1, message, 15, 14)
+
+app = App("Hello World", bg='black', width=800, height=600, layout="grid")
+players = []
+
+def start_game(player_count):
+    clear_screen()
+
+    message_index = randint(0, len(phrases) - 1)
+    message = phrases[message_index]
+
+    Text(app, text=message, size=font_size, font=font, color=phrase_color, grid=[0,0, 3,1],align='top')
+    Text(app, text="1 - Undo     0 - Space", size=int(font_size/2), font=font, color=phrase_color, grid=[2,10],align='bottom')
+
+
+    for i in range(player_count):
+        players.append(Player(i, message, 1,1))
+
+def clear_screen():
+    for child in app.children:
+        child.clear()
+        child.hide()
+        del child
+    
+    app.children.clear()
+
+def show_start_screen():
+    clear_screen()
+    Text(app, text="Welcome to Rotary Phone Texting", size=100, font=font, grid=[0,0] , color=main_font_color,align='center')
+
+def clicked(key):
+    print(key.key)
+    if key.key == 'q':
+        show_start_screen()
+        return
+
+    if key.key == '1' or key.key == '2' or key.key == '3' or key.key == '4':
+        start_game(int(key.key))
+        return
+
+
+app.when_key_pressed = clicked
+show_start_screen()
+app.display()
